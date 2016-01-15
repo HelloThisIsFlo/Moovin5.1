366c9b0cc36384abfef34ee03779f055719dfec7
Florian Kempenich <ShockN745@gmail.com>
2016-01-15 22:09:17 +0100

    Add first activity + basic elements

---
+++ app/src/main/java/com/shockn745/moovin5/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shockn745.moovin5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
